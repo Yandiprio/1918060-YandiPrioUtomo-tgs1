f54cd132d81c6db7b2b0cb86436e332551ede213
yandiprio <yandiprio134@gmail.com>
2021-10-14 22:02:12 -0800

    first commit

---
+++ app/src/main/java/com/example/a1918060_tugaslayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1918060_tugaslayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
